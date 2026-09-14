5aabab142a5a1133e39d22e41b8e432334ef2abc
dadalili2019 <54031455+dadalili2019@users.noreply.github.com>
2026-09-14 16:13:27 +0800

    feat(workbench): add knowledge repository contract

---
+++ lib/workbench/domain/knowledge_repository.dart
@@ -0,0 +1,9 @@
+import '../core/models.dart';
+
+abstract interface class KnowledgeRepository {
+  Future<List<KnowledgeModel>> listActive({String? category});
+  Future<KnowledgeModel?> getById(String id);
+  Future<List<KnowledgeModel>> getByIds(List<String> ids);
+  Future<void> insert(KnowledgeModel knowledge);
+  Future<void> update(KnowledgeModel knowledge);
+}
