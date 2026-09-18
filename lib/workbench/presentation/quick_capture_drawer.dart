0d1f0386ae385393de1191db941012ace10ec0a5
dadalili2019 <54031455+dadalili2019@users.noreply.github.com>
2026-09-18 13:57:35 +0800

    fix(workbench): add theme imports for v1.4 surfaces

--- lib/workbench/presentation/quick_capture_drawer.dart
+++ lib/workbench/presentation/quick_capture_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 
+import '../../theme/theme_controller.dart';
 import '../core/models.dart';
 import '../workbench_runtime.dart';
 import 'workbench_ui.dart';
